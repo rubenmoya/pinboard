b1b7597c763656ad0dd747583dbf838979cc0df5
rubenmoya <rmoyarodriguez@gmail.com>
2015-06-10 19:04:18 +0200

    Forms styling.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,4 +1,5 @@
 class PinsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
 
   def index
